bc6f485bf0d0aad17ed2cb3204adb78d3d32529e
samnyan <4137880+samnyan@users.noreply.github.com>
2025-08-01 22:45:53 +0800

    feat(web): implement dark mode and introduce custom theme
    - Add dark mode support using primevue theme options
    - Create custom theme 'Noir' based on Aura
    - Update main.ts to use new theme and configure dark mode
    - Add primeicons dependency

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,15 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 import 'virtual:uno.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura';
 
 import App from './App.vue'
 import router from './router'
+import { Noir } from '@/theme.ts'
 
 const app = createApp(App)
 
@@ -17,8 +18,11 @@ app.use(router)
 
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
-  }
+    preset: Noir,
+    options: {
+      darkModeSelector: 'system',
+    },
+  },
 })
 
 app.mount('#app')
